6748eff9bb0bc7c7c62a4949e12401cc723e2d64
razzaksr <razzaksr@gmail.com>
2023-08-29 11:47:10 +0530

    Template to Flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,make_response, render_template
+from flask import Flask,make_response, render_template, request
 
 app=Flask(__name__)
 
@@ -14,6 +14,31 @@ def askMe():
 @app.route("/page1")
 def tempFirst():
     return render_template("first.html")
+
+@app.route("/send",methods=['GET','POST'])
+def performSignUp():
+    if request.method=="GET":
+        return render_template("bootforms.html")
+    else:
+        firstName=request.form['fname']
+        lastName=request.form['lname']
+        email=request.form['mail']
+        contact=request.form['mobile']
+        print(firstName,lastName,contact,email)
+        return render_template("bootforms.html",msg=firstName+" account created")
+
+# @app.route("/signup")
+# def showSignUp():
+#     return render_template("bootforms.html")
+
+
+# @app.route("/send",methods=['POST'])
+# def receiveSignUp():
+#     firstName=request.form['fname']
+#     lastName=request.form['lname']
+#     email=request.form['mail']
+#     contact=request.form['mobile']
+#     print(firstName,lastName,contact,email)
 
 if __name__=="__main__":
     app.run(debug=True,port=1111)
